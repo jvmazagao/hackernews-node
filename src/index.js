b35a49f75ff7a01bcbf5be577f842ed00996b7c7
Joao Victor <joao.victor@rarolabs.com.br>
2020-10-16 14:44:23 -0300

    ïnitial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { GraphQLServer } = require("graphql-yoga");
+const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require("graphql-yoga");
+
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require('./resolvers/Vote')
+
+const prisma = new PrismaClient();
+const pubsub = new PubSub();
+
+// 1
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+  Vote
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: (request) => {
+    return {
+      ...request,
+      prisma,
+      pubsub,
+    };
+  },
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
